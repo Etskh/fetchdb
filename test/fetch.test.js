6efa6c36b5f5d83cedef3c681837c6de5171c3fe
james.loucks <james.loucks@dominionmarinemedia.com>
2016-12-06 18:42:28 -0800

    Huge update

--- test/fetch.test.js
+++ test/fetch.test.js
@@ -7,7 +7,45 @@ const expect = require('expect');
 
 describe('lib / fetchdb', function() {
 
-  const fetchdb = require('../lib/fetchdb');
+  it('can be initialised', function(done) {
+    const fetchdb = require('../lib/fetch2');
+    const crypt = (str) => {
+      return str;
+    };
+    const userModelConfig = {
+      schema: {
+        username: 'string',
+        password: 'string',
+      },
+      methods: {
+        authenticate: (password) => {
+          return this.password === crypt(password);
+        },
+      },
+    };
+
+    // just sets things and returns immediately
+    // uses default memory database
+    const userModel = fetchdb('user', userModelConfig );
+
+    expect(userModel.data).toEqual(userModelConfig);
+    expect(userModel.name).toEqual('user');
+    expect(userModel.connection).toExist();
+    expect(userModel.methods).toExist();
+
+    // Create a new entry in the model
+    userModel.create({
+      username: 'admin',
+      password: 'pass1',
+    }).then( (userController) => {
+      // check userController
+      expect(userController.authenticate).toExist();
+
+      done();
+    });
+  });
+
+  /*const fetchdb = require('../lib/fetchdb');
   const modelName = 'model';
   const model = fetchdb(modelName);
 
@@ -26,7 +64,7 @@ describe('lib / fetchdb', function() {
 
   it('inmemory database can add new items', function(done) {
     const toInsert = {
-      name: 'some data'
+      name: 'some data',
     };
     const db = require('../lib/connectors/memory');
     const oldCount = db(modelName).count();
@@ -36,6 +74,5 @@ describe('lib / fetchdb', function() {
     toInsert.id = insertedElement.id;
     expect(insertedElement).toEqual(toInsert);
     done();
-  });
-
+  });*/
 });
