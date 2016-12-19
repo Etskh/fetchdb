3c68658b6ee65b4a4c701d1804a05d0cb1d51766
Etskh <etskh@hotmail.com>
2016-12-19 15:43:53 -0800

    Updated system

--- test/fetch.test.js
+++ test/fetch.test.js
@@ -7,72 +7,65 @@ const expect = require('expect');
 
 describe('lib / fetchdb', function() {
 
-  it('can be initialised', function(done) {
-    const fetchdb = require('../lib/fetch2');
-    const crypt = (str) => {
-      return str;
-    };
-    const userModelConfig = {
-      schema: {
-        username: 'string',
-        password: 'string',
+  const fetchdb = require('../lib/fetch2');
+  const crypt = (str) => {
+    return str;
+  };
+  const userModelConfig = {
+    db: {
+      type: 'memdb'
+    },
+    schema: {
+      username: 'string',
+      password: 'string',
+    },
+    methods: {
+      authenticate: (self, password) => {
+        return self.password === crypt(password);
       },
-      methods: {
-        authenticate: (password) => {
-          return this.password === crypt(password);
-        },
-      },
-    };
+    },
+  };
+  const userData = {
+    username: 'admin',
+    password: 'pass1',
+  };
 
-    // just sets things and returns immediately
-    // uses default memory database
-    const userModel = fetchdb('user', userModelConfig );
+  describe('.()', function() {
+    it('can be initialised', function(done) {
 
-    expect(userModel.data).toEqual(userModelConfig);
-    expect(userModel.name).toEqual('user');
-    expect(userModel.connection).toExist();
-    expect(userModel.methods).toExist();
+      // just sets things and returns immediately
+      // uses default memory database
+      const userModel = fetchdb('user', userModelConfig );
 
-    // Create a new entry in the model
-    userModel.create({
-      username: 'admin',
-      password: 'pass1',
-    }).then( (userController) => {
-      // check userController
-      expect(userController.authenticate).toExist();
+      expect(userModel.data).toEqual(userModelConfig);
+      expect(userModel.name).toEqual('user');
+      expect(userModel.connection).toExist();
+      expect(userModel.methods).toExist();
 
       done();
     });
   });
+  describe('.create()', function() {
+    it('can create a controller with methods and fields', function(done) {
+      // just sets things and returns immediately
+      // uses default memory database
+      const userModel = fetchdb('user', userModelConfig );
 
-  /*const fetchdb = require('../lib/fetchdb');
-  const modelName = 'model';
-  const model = fetchdb(modelName);
-
-  it('can call info that returns an info block about the state', function(done) {
-
-    // create a new connector and model without config
-    model.info().then( (info) => {
-      expect(info).toEqual({
-        type: 'memdb',
-        name: modelName,
-        count: 0,
+      // Create a new entry in the model
+      userModel.create(userData).then( (userController) => {
+        // check userController
+        // It adds the methods that we pass it
+        expect(userController.id).toExist();
+        expect(userController.username).toEqual(userData.username);
+        expect(userController.password).toEqual(userData.password);
+        // Make sure custom controller methods exist and work
+        expect(userController.authenticate).toExist();
+        expect(userController.authenticate(userData.password)).toEqual(true);
+        // Make sure built-in controller methods work
+        expect(userController.save).toExist();
+        done();
       });
-      done();
     });
   });
 
-  it('inmemory database can add new items', function(done) {
-    const toInsert = {
-      name: 'some data',
-    };
-    const db = require('../lib/connectors/memory');
-    const oldCount = db(modelName).count();
-    const insertedElement = db(modelName).insert(toInsert);
-
-    expect(db(modelName).count()).toEqual( oldCount + 1);
-    toInsert.id = insertedElement.id;
-    expect(insertedElement).toEqual(toInsert);
-    done();
-  });*/
 });
---
+++ test/lib/connectors/memory.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const expect = require('expect');
+/*
+describe('lib / connectors / memory', function() {
+
+  const fetchdb = require('../lib/fetchdb');
+  const modelName = 'model';
+  const model = fetchdb(modelName);
+
+  describe('#', function() {
+    it('will create a connector with ', function(done) {
+
+      // create a new connector and model without config
+      model.info().then( (info) => {
+        expect(info).toEqual({
+          type: 'memdb',
+          name: modelName,
+          count: 0,
+        });
+        done();
+      });
+    });
+  });
+});
+*/
